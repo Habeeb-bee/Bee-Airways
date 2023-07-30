cd9b3d87c7e60352555c8b91edaaa2d92d7a9da2
Habeeb-bee <138727069+Habeeb-bee@users.noreply.github.com>
2023-07-30 02:22:20 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var submit = document.querySelector(".flight-btn");
 var pop = alert('You have successfully booked a flight with BEE AIRWAYS');
-submit.addEventListener('click', (e) =>{
+submit.addEventListener("click", function e()
+{
+    
     console.log(pop);
-} )
+
+} )  
